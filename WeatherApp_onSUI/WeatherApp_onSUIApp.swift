62ba4163aafb793289931f8fe3dce339fff288cf
Maxim <swiftdevfromsiberia@gmail.com>
2024-03-09 20:36:59 +0500

    Initial Commit

---
+++ WeatherApp_onSUI/WeatherApp_onSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_onSUIApp.swift
+//  WeatherApp_onSUI
+//
+//  Created by Makarov_Maxim on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_onSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
