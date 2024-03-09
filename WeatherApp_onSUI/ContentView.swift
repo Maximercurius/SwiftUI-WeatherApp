8dbcac4bd63a583f5a40c4711e311f8db5275e3a
Maxim <swiftdevfromsiberia@gmail.com>
2024-03-09 21:19:37 +0500

    Initial

--- WeatherApp_onSUI/ContentView.swift
+++ WeatherApp_onSUI/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.white, .blue, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Borovsky")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.blue)
+                    .padding()
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
